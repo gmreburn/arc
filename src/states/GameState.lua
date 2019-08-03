41d1614f0f7e265548033e5a570a676d80a33239
Gregory Reburn <gmreburn@gmail.com>
2019-08-03 15:12:03 -0500

    limit FPS 30

--- src/states/GameState.lua
+++ src/states/GameState.lua
@@ -7,7 +7,11 @@ local KeyPressed = require("events/KeyPressed")
 -- State superclass
 local State = require("core/State")
 local GameState = class("GameState", State)
+
 function GameState:load()
+    min_dt = 1/30
+    self.next_time = love.timer.getTime()
+
     self.engine = Engine()
     self.eventmanager = EventManager()
 
@@ -17,10 +21,19 @@ end
 
 function GameState:update(dt)
     self.engine:update(dt)
+    self.next_time = self.next_time + min_dt
 end
 
 function GameState:draw()
     self.engine:draw()
+
+    local cur_time = love.timer.getTime()
+    if self.next_time <= cur_time then
+        self.next_time = cur_time
+        return
+    end
+
+   love.timer.sleep(self.next_time - cur_time)
 end
 
 function GameState:keypressed(key, isrepeat)
