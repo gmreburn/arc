a470a3e5b2b84564a40e6595ac02caae5bea9b92
Gregory Reburn <gmreburn@gmail.com>
2019-08-10 17:39:36 -0500

    send mock connect info to client

--- arc-server/app.js
+++ arc-server/app.js
@@ -1,14 +1,13 @@
+var arc = require('./constants');
 const uuidv4 = require('uuid/v4');
 const enet = require("enet");
 const md5 = require("md5");
 const addr = new enet.Address("0.0.0.0", 3000);
 
-const GAME_INITIALIZE = 1;
-
 var p1 = uuidv4();
 var players = {
   p1: {
-    position: {x:0,y:0}
+    position: { x: 0, y: 0, vx: 0, xy: 0 }
   }
 }
 
@@ -27,7 +26,16 @@ enet.createServer({
   //setup event handlers..
   host.on("connect", function (peer, data) {
     var payload = {
-      type: GAME_INITIALIZE,
+      type: arc.GAME_INITIALIZE,
+      team: arc.TEAM_GREEN,
+      position: { x: 1, y: 1 },
+      map: {
+        name: 'aplgo.map',
+        hash: md5('file contents.map'),
+        energyRechargeRate: arc.ENERGY_RECHARGE_RATE,
+        specialRechargeRate: arc.SPECIAL_RECHARGE_RATE,
+        objective: arc.OBJECTIVE_CTF
+      },
       secret: uuidv4(),
       players: {}
     };
@@ -38,11 +46,30 @@ enet.createServer({
     });
     peer.send(0, JSON.stringify(payload));
     console.log("sent connect packet:", JSON.stringify(payload));
-    
+
 
     //incoming peer connection
     peer.on("message", function (packet, channel) {
       console.log("received packet contents:", packet.data().toString());
+      const msg = JSON.parse(packet.data());
+      switch (msg.type) {
+        case arc.FIRE_LASER:
+          break;
+        case arc.FIRE_MISSILE:
+          break;
+        case arc.FIRE_MORTAR:
+          break;
+        case arc.FIRE_BOUNCY:
+          break;
+        case arc.TEAM_CHANGED:
+          break;
+        case arc.VELOCITY_CHANGED:
+          break;
+        case arc.FLAG_CAPTURED:
+          break;
+        case arc.SWITCH_FLIPPED:
+          break;
+      }
     });
   });
 
