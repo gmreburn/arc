48a7d23f1735238b3e5a320cc17ceca74bfff3f3
Gregory Reburn <gmreburn@gmail.com>
2019-08-10 16:58:15 -0500

    add constants

---
+++ arc-server/constants.js
@@ -0,0 +1,55 @@
+module.exports = Object.freeze({
+    TEAM_GREEN: 0,
+    TEAM_RED: 1,
+    TEAM_BLUE: 2,
+    TEAM_YELLOW: 3,
+    TEAM_NEUTRAL: 4,
+
+    ENERGY_RECHARGE_RATE: 2, // map can impact this rate
+    SPECIAL_RECHARGE_RATE: .05, // each special has its own recharge rate and the map can also impact this rate
+
+    MAX_ENERGY: 10,
+    MAX_HEALTH: 10,
+    MAX_MISSILES: 3,
+    MAX_GRENADES: 2,
+    MAX_BOUNCIES: 3,
+
+    WEAPON_LASER: 0,
+    WEAPON_MISSILE: 1,
+    WEAPON_GRENADE: 2,
+    WEAPON_BOUNCY: 3,
+
+    HEADING_EAST: 1,
+    HEADING_NORTHEAST: 2,
+    HEADING_NORTH: 3,
+    HEADING_NORTHWEST: 4,
+    HEADING_WEST: 5,
+    HEADING_SOUTHWEST: 6,
+    HEADING_SOUTH: 7,
+    HEADING_SOUTHEAST: 8,
+    HEADING_NONE: 9,
+
+    OBJECTIVE_CTF: 0,
+    OBJECTIVE_TOGGLE_SWITCHES: 1,
+
+    FLAG_SIZE: 12,
+    TILE_SIZE: 16,
+    POWERUP_SIZE: 24,
+    SHIP_SIZE: 32,
+    SHIP_VELOCITY: 30,
+    BOUNCY_VELOCITY: 30,
+    MISSILE_VELOCITY: 30,
+    MORTAR_VELOCITY: 30,
+    LASER_VELOCITY: 30,
+
+    // NETWORK
+    GAME_INITIALIZE: 1,
+    FIRE_LASER: 2,
+    FIRE_MISSILE: 3,
+    FIRE_MORTAR: 4,
+    FIRE_BOUNCY: 5,
+    TEAM_CHANGED: 6,
+    VELOCITY_CHANGED: 7,
+    FLAG_CAPTURED: 8,
+    SWITCH_FLIPPED: 9
+});
