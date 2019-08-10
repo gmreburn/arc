1e7f93c728b36d2adf6186e6f0e09eabc0e92885
Gregory Reburn <gmreburn@gmail.com>
2019-08-10 17:41:13 -0500

    add ship to engine

--- src/systems/NetworkSystem.lua
+++ src/systems/NetworkSystem.lua
@@ -6,6 +6,8 @@ local enet = require "enet"
 local host = enet.host_create()
 local playerid = nil
 local server = nil
+local ShipEntity = require('entities/ShipEntity')
+local md5 = require('lib/md5')
 
 function NetworkSystem:onPrimaryWeaponFired(playerid, x, y, theta)
     -- send update to socket
@@ -17,30 +19,26 @@ function NetworkSystem:initialize()
 end
 
 function NetworkSystem:update(dt)
-    if(playerid) then
-        local event = host:service(100)
-        if (event) then
-            if event.type == "receive" then
-                local message = json.decode(event.data)
-                if(message.type == GAME_INITIALIZE) then
-                    print("Got message: ", event.data, event.peer)
-                    playerid = message.secret
-                    print(playerid)
-                end
-            elseif event.type == "connect" then
-                print(event.peer, "connected.")
-                -- event.peer:send( "setUserName", "gmrdeburn" )
-            elseif event.type == "disconnect" then
-                print(event.peer, "disconnected.")
-            else
-                print('idk this event '..event.type)
+    local event = host:service(100)
+    if (event) then
+        if event.type == "receive" then
+            local message = json.decode(event.data)
+            if(message.type == GAME_INITIALIZE) then
+                print("Got message: ", event.data, event.peer)
+
+                stack:current().engine:addEntity(ShipEntity(md5.sumhexa(message.secret), tonumber(message.position.x), tonumber(message.position.y), tonumber(message.team)))
+ 
+                -- iterate the player list to add other entities
             end
+        elseif event.type == "connect" then
+            print(event.peer, "connected.")
+            -- event.peer:send(json.encode({ type = GAME_INITIALIZE, username = "gmreburn" }))
+        elseif event.type == "disconnect" then
+            print(event.peer, "disconnected.")
+        else
+            print('idk this event '..event.type)
         end
     end
-end
-
-function serialize (o)
-    
 end
 
 return NetworkSystem
