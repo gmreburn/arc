c884556cac6f5739e090864e1caead7872291163
Gregory Reburn <gmreburn@gmail.com>
2019-08-10 22:32:30 -0500

    format mousepressed

--- src/events/MousePressed.lua
+++ src/events/MousePressed.lua
@@ -1,9 +1,9 @@
 local MousePressed = class("MousePressed")
 
 function MousePressed:initialize(x, y, button)
-    self.button = button
-    self.y = y
     self.x = x
+    self.y = y
+    self.button = button
 end
 
 return MousePressed
