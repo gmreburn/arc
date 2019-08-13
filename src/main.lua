ca89b1fed84c7d780fabf94330a7e9313fd42178
Gregory Reburn <gmreburn@gmail.com>
2019-08-12 19:28:07 -0500

    limit FPS to 30

--- src/main.lua
+++ src/main.lua
@@ -11,18 +11,29 @@ require("core/Resources")
 local InitializingState = require("states/InitializingState")
 
 function love.load()
+    min_dt = 1 / 30
+    self.next_time = love.timer.getTime()
+
     stack = StackHelper()
-    -- stack:push(GameState())
     stack:push(InitializingState())
 end
 
 function love.update(dt)
     require("lib/lovebird").update()
     stack:current():update(dt)
+    self.next_time = self.next_time + min_dt
 end
 
 function love.draw()
     stack:draw()
+
+    local cur_time = love.timer.getTime()
+    if self.next_time <= cur_time then
+        self.next_time = cur_time
+        return
+    end
+
+   love.timer.sleep(self.next_time - cur_time)
 end
 
 function love.keypressed(key, isrepeat)
